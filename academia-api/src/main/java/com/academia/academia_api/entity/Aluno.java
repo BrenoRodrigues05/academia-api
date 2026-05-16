94fa895c46c06a7bbcf4173f42605510aadfaf9e
BrenoRodrigues05 <brenobr10@hotmail.com>
2026-05-15 21:51:11 -0300

    adiciona a entidade Alunos

---
+++ academia-api/src/main/java/com/academia/academia_api/entity/Aluno.java
@@ -0,0 +1,27 @@
+package com.academia.academia_api.entity;
+
+import com.academia.academia_api.entity.enums.SexoEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "alunos")
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private int  id;
+   private String nome;
+   private String email;
+   private int idade;
+   private String telefone;
+    @Enumerated(EnumType.STRING)
+   private SexoEnum sexo;
+}
