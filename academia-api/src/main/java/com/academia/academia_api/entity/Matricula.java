9c3f2d3f87acf5d7adc7f7a651a74555ff070910
BrenoRodrigues05 <brenobr10@hotmail.com>
2026-05-15 22:18:10 -0300

    adiciona a entidade Matriculas

---
+++ academia-api/src/main/java/com/academia/academia_api/entity/Matricula.java
@@ -0,0 +1,26 @@
+package com.academia.academia_api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "matriculas")
+public class Matricula {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String matricula;
+    @ManyToOne
+    @JoinColumn(name = "plano_id", nullable = false)
+    private Plano plano;
+    @ManyToOne
+    @JoinColumn(name = "aluno_id", nullable = false)
+    private Aluno aluno;
+    @Column(nullable = false)
+    private boolean ativa;
+}
