29dcf22228917f53591da2a785aca756ffb87fab
BrenoRodrigues05 <brenobr10@hotmail.com>
2026-05-15 22:06:09 -0300

    adiciona a entidade Plano

---
+++ academia-api/src/main/java/com/academia/academia_api/entity/Plano.java
@@ -0,0 +1,28 @@
+package com.academia.academia_api.entity;
+
+import com.academia.academia_api.entity.enums.TipoPlano;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "planos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Plano {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String descricao;
+    private BigDecimal valor;
+    @Enumerated(EnumType.STRING)
+    private TipoPlano tipo;
+}
