a8f00c47731cea2eaf72b33550a7f9a03da97097
BrenoRodrigues05 <brenobr10@hotmail.com>
2026-05-18 21:57:18 -0300

    adiciona repository para Plano

---
+++ academia-api/src/main/java/com/academia/academia_api/repository/PlanoRepository.java
@@ -0,0 +1,9 @@
+package com.academia.academia_api.repository;
+
+import com.academia.academia_api.entity.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlanoRepository extends JpaRepository<Aluno, Long> {
+}
