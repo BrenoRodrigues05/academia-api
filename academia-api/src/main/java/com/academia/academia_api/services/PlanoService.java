0b367e77d5fac017b1a9f40bd8c1f36267addfac
BrenoRodrigues05 <brenobr10@hotmail.com>
2026-06-02 19:53:22 -0300

    refatora service de Planos para usar mapper

--- academia-api/src/main/java/com/academia/academia_api/services/PlanoService.java
+++ academia-api/src/main/java/com/academia/academia_api/services/PlanoService.java
@@ -1,7 +1,10 @@
 package com.academia.academia_api.services;
-
+import com.academia.academia_api.DTOs.PlanoCreateDTO;
+import com.academia.academia_api.DTOs.PlanoResponseDTO;
+import com.academia.academia_api.DTOs.PlanoUpdateDTO;
 import com.academia.academia_api.entity.Plano;
 import com.academia.academia_api.entity.enums.TipoPlano;
+import com.academia.academia_api.mappings.PlanoMapper;
 import com.academia.academia_api.repository.PlanoRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,69 +15,116 @@ import java.util.List;
 public class PlanoService {
 
     private final PlanoRepository planoRepository;
+    private final PlanoMapper planoMapper;
 
-    public PlanoService(PlanoRepository planoRepository) {
+    public PlanoService(PlanoRepository planoRepository, PlanoMapper planoMapper) {
         this.planoRepository = planoRepository;
+        this.planoMapper = planoMapper;
     }
 
-    public List<Plano> findAll() {
-        return planoRepository.findAll();
+    public List<PlanoResponseDTO> findAll() {
+        return planoRepository.findAll().stream()
+                .map(planoMapper::toResponseDTO)
+                .toList();
     }
 
-    public Plano findById(Long id) {
+    public PlanoResponseDTO findById(Long id) {
         if(id == null || id <= 0) {
             throw new RuntimeException("Id invalido ou inexistente");
         }
-        return  planoRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Aluno não encontrado."));
+        Plano plano = planoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Id invalido ou inexistente"));
+
+        return  planoMapper.toResponseDTO(plano);
     }
 
-    public Plano criarPlano(Plano plano) {
+    public PlanoResponseDTO criarPlano(PlanoCreateDTO dto) {
 
-        return planoRepository.save(plano);
+        Plano  plano = planoMapper.toEntity(dto);
+        plano = planoRepository.save(plano);
+        return planoMapper.toResponseDTO(plano);
     }
 
-    public Plano atualizarPlano(Plano plano) {
-        return planoRepository.save(plano);
+    public PlanoResponseDTO atualizarPlano(Long id, PlanoUpdateDTO updateDTO) {
+
+        if(id == null || id <= 0) {
+            throw new RuntimeException("Id invalido ou inexistente");
+        }
+
+        var buscaPlano =  planoRepository.findById(id);
+        if(buscaPlano == null) {
+            throw new RuntimeException("Id invalido ou inexistente");
+        }
+
+        Plano planoExistente = buscaPlano.orElseThrow(() -> new RuntimeException("Id invalido ou inexistente"));
+
+        planoMapper.updateEntityFromDTO(updateDTO,  planoExistente);
+
+        Plano  planoAtualizado = planoRepository.save(planoExistente);
+
+        return  planoMapper.toResponseDTO(planoAtualizado);
     }
 
-    public void deletarPlano(Long id) {
+    public PlanoResponseDTO deletarPlano(Long id) {
         if(id == null || id <= 0) {
             throw new RuntimeException("Id inexistente ou inválido.");
         }
-        planoRepository.deleteById(id);
+
+        Plano planoDeletado = planoRepository.findById(id).orElse(null);
+
+        if(planoDeletado != null) {
+            planoRepository.delete(planoDeletado);
+        }
+
+        return planoMapper.toResponseDTO(planoDeletado);
     }
 
-    public Plano BuscarPeloNome(String nome) {
-        if(nome == null || nome.isEmpty()) {
+    public PlanoResponseDTO BuscarPeloNome(String nome) {
+        if(nome == null || nome.isBlank()) {
             throw new RuntimeException("Nome do plano inválido ou não existe.");
         }
-        return planoRepository.findByNome(nome);
+        Plano plano = planoRepository.findByNome(nome);
+
+        if (plano == null) {
+            throw new RuntimeException("Plano não encontrado.");
+        }
+
+        return planoMapper.toResponseDTO(plano);
     }
 
-    public Plano  BuscarPeloDescricao(String descricao) {
-        if(descricao == null || descricao.isEmpty()) {
+    public PlanoResponseDTO  BuscarPeloDescricao(String descricao) {
+        if(descricao == null || descricao.isBlank()) {
             throw new RuntimeException("Descrição inválida ou não existe.");
         }
-        return  planoRepository.findByDescricao(descricao);
-    }
 
-    public Plano  BuscarPeloTipo(TipoPlano tipo) {
-        var bucaPlano = planoRepository.findByTipo(tipo);
-        if(bucaPlano == null) {
-            throw new RuntimeException("Não existe plano com o tipo selecionado.");
+        Plano plano = planoRepository.findByDescricao(descricao);
+
+        if (plano == null) {
+            throw new RuntimeException("Plano não encontrado.");
         }
-        return bucaPlano;
+
+        return planoMapper.toResponseDTO(plano);
     }
 
-    public Plano BuscaPeloValor(BigDecimal valor) {
+    public PlanoResponseDTO  BuscarPeloTipo(TipoPlano tipo) {
+
+        Plano plano = planoRepository.findByTipo(tipo);
+
+        if (plano == null) {
+            throw new RuntimeException("Plano não encontrado.");
+        }
+
+        return planoMapper.toResponseDTO(plano);
+    }
+
+    public PlanoResponseDTO BuscaPeloValor(BigDecimal valor) {
         if(valor == null || valor.compareTo(BigDecimal.ZERO) <= 0) {
             throw new RuntimeException("Valor inválido ou nulo");
         }
-        var bucaPlano = planoRepository.findByValor(valor);
-        if(bucaPlano == null) {
+        Plano plano = planoRepository.findByValor(valor);
+        if(plano == null) {
             throw new RuntimeException("Não existe plano com o valor selecionado.");
         }
-        return bucaPlano;
+        return planoMapper.toResponseDTO(plano);
     }
 }
