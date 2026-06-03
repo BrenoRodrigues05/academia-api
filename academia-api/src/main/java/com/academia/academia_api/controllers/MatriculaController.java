55bfedb25ecd3ed5fd427884ba716a1fced06873
BrenoRodrigues05 <brenobr10@hotmail.com>
2026-06-02 21:06:55 -0300

    cria controller para matriculas

---
+++ academia-api/src/main/java/com/academia/academia_api/controllers/MatriculaController.java
@@ -0,0 +1,51 @@
+package com.academia.academia_api.controllers;
+import com.academia.academia_api.DTOs.MatriculaCreateDTO;
+import com.academia.academia_api.DTOs.MatriculaResponseDTO;
+import com.academia.academia_api.services.MatriculaService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/matriculas")
+public class MatriculaController {
+
+    private final MatriculaService matriculaService;
+
+    public MatriculaController(MatriculaService matriculaService) {
+        this.matriculaService = matriculaService;
+    }
+
+   @GetMapping
+    public ResponseEntity<List<MatriculaResponseDTO>> matriculas() {
+        return ResponseEntity.ok(matriculaService.listarMatriculas());
+   }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MatriculaResponseDTO> getMatricula(@PathVariable Long id) {
+        return ResponseEntity.ok(matriculaService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<MatriculaResponseDTO> criarMatricula(
+            @Valid @RequestBody MatriculaCreateDTO dto,
+            @RequestParam Long idAluno,
+            @RequestParam Long idPlano) {
+
+        MatriculaResponseDTO novaMatricula =
+                matriculaService.criarMatricula(dto, idAluno, idPlano);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(novaMatricula);
+    }
+
+    @PatchMapping("/{id}/desativar")
+    public ResponseEntity<MatriculaResponseDTO> deletarMatricula(@PathVariable Long id){
+        MatriculaResponseDTO matriculaDesativada = matriculaService.desativarMatricula(id);
+        return ResponseEntity.ok(matriculaDesativada);
+    }
+}
