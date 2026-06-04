01c8f39563d4330fe16c9a1d235e1dca0b1f14ce
BrenoRodrigues05 <brenobr10@hotmail.com>
2026-06-04 19:55:08 -0300

    cria repository para Personal.

---
+++ academia-api/src/main/java/com/academia/academia_api/repository/PersonalRepository.java
@@ -0,0 +1,17 @@
+package com.academia.academia_api.repository;
+
+import com.academia.academia_api.entity.Personal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PersonalRepository extends JpaRepository<Personal, Long> {
+    Optional<Personal> findByEmail(String email);
+    Optional<Personal> findByCref(String cref);
+    List<Personal> findByNome(String nome);
+    List<Personal> findByAtivoFalse();
+    List<Personal> findByAtivoTrue();
+}
