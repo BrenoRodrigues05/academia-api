b22845cc3ae9581672d0c5dcb279afd283857563
BrenoRodrigues05 <brenobr10@hotmail.com>
2026-06-05 19:35:58 -0300

    cria repository para Treinos.

---
+++ academia-api/src/main/java/com/academia/academia_api/repository/TreinoRepository.java
@@ -0,0 +1,27 @@
+package com.academia.academia_api.repository;
+
+import com.academia.academia_api.entity.Treino;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TreinoRepository extends JpaRepository<Treino, Long> {
+    List<Treino> findByAtivoTrue();
+
+    List<Treino> findByAtivoFalse();
+
+    List<Treino> findByNomeContainingIgnoreCase(String nome);
+
+    List<Treino> findByPersonalId(Long personalId);
+
+    List<Treino> findByAlunoId(Long alunoId);
+
+    Optional<Treino> findByAlunoIdAndAtivoTrue(Long alunoId);
+
+    List<Treino> findByPersonalIdAndAtivoTrue(Long personalId);
+
+    boolean existsByAlunoIdAndAtivoTrue(Long alunoId);
+}
