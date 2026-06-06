50d4f287cf5181ed7cd9529ef93162244635b45c
BrenoRodrigues05 <brenobr10@hotmail.com>
2026-06-05 21:28:14 -0300

    cria controller para Exercício.

---
+++ academia-api/src/main/java/com/academia/academia_api/controllers/ExercicioController.java
@@ -0,0 +1,66 @@
+package com.academia.academia_api.controllers;
+
+import com.academia.academia_api.DTOs.ExercicioCreateDTO;
+import com.academia.academia_api.DTOs.ExercicioResponseDTO;
+import com.academia.academia_api.DTOs.ExercicioUpdateDTO;
+import com.academia.academia_api.services.ExercicioService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/exercicios")
+public class ExercicioController {
+
+    private final ExercicioService exercicioService;
+
+    public ExercicioController(ExercicioService exercicioService) {
+        this.exercicioService = exercicioService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ExercicioResponseDTO>> findAll() {
+        return ResponseEntity.ok(exercicioService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ExercicioResponseDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(
+                exercicioService.findById(id));
+    }
+
+    @GetMapping("/busca-nome")
+    public ResponseEntity<List<ExercicioResponseDTO>> findByNome(@RequestParam String nome) {
+        return ResponseEntity.ok(
+                exercicioService.findByNome(nome));
+    }
+
+    @GetMapping("/grupo-muscular")
+    public ResponseEntity<List<ExercicioResponseDTO>> findByGrupoMuscular(@RequestParam String grupoMuscular) {
+        return ResponseEntity.ok(
+                exercicioService.findByGrupoMuscular(grupoMuscular));
+    }
+
+    @PostMapping
+    public ResponseEntity<ExercicioResponseDTO> addExercicio(@Valid @RequestBody ExercicioCreateDTO dto) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(exercicioService.addExercicio(dto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ExercicioResponseDTO> updateExercicio(@PathVariable Long id, @Valid @RequestBody ExercicioUpdateDTO dto)
+    {
+        return ResponseEntity.ok(
+                exercicioService.updateExercicio(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ExercicioResponseDTO> deleteExercicio(@PathVariable Long id) {
+        return ResponseEntity.ok(
+                exercicioService.deleteExercicio(id));
+    }
+}
