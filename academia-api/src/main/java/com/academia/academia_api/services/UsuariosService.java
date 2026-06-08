767e86dc91b96f4f322431484900a185bd44f331
BrenoRodrigues05 <brenobr10@hotmail.com>
2026-06-08 19:43:16 -0300

    cria repository e service para Usuarios.

---
+++ academia-api/src/main/java/com/academia/academia_api/services/UsuariosService.java
@@ -0,0 +1,19 @@
+package com.academia.academia_api.services;
+import com.academia.academia_api.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuariosService implements UserDetailsService {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.UserDetailsfindByLogin(username);
+    }
+}
