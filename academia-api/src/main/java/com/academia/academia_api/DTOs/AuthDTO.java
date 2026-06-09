daab7a96c0127360fc12684ce5990c067248a4de
BrenoRodrigues05 <brenobr10@hotmail.com>
2026-06-08 21:27:51 -0300

    adiciona estrutura inicial de autenticação e autorização

---
+++ academia-api/src/main/java/com/academia/academia_api/DTOs/AuthDTO.java
@@ -0,0 +1,5 @@
+package com.academia.academia_api.DTOs;
+
+public record AuthDTO(String login, String senha) {
+
+}
---
+++ academia-api/src/main/java/com/academia/academia_api/controllers/AuthController.java
@@ -0,0 +1,29 @@
+package com.academia.academia_api.controllers;
+import com.academia.academia_api.DTOs.AuthDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity login (@RequestBody @Valid AuthDTO data) {
+
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.senha());
+        var authentication = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
