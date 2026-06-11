abc45b0c1a0eda3550ba935e1903c26679670be8
BrenoRodrigues05 <brenobr10@hotmail.com>
2026-06-10 21:38:09 -0300

    ajusta retorno do login.

--- academia-api/src/main/java/com/academia/academia_api/services/UsuariosService.java
+++ academia-api/src/main/java/com/academia/academia_api/services/UsuariosService.java
@@ -10,10 +10,14 @@ import org.springframework.stereotype.Service;
 public class UsuariosService implements UserDetailsService {
 
     @Autowired
-    UsuarioRepository usuarioRepository;
+    private UsuarioRepository usuarioRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return usuarioRepository.findByLogin(username);
+        var usuario = usuarioRepository.findByLogin(username);
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Usuário não encontrado");
+        }
+        return usuario;
     }
 }
